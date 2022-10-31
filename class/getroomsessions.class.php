dc8452312e3a030f5a70f32ad396eb897c87f35c
unknown <chelvibenjamin@gmail.com>
2022-10-31 17:58:35 +0100

    affichage des parties d'une room

--- class/getroomsessions.class.php
+++ class/getroomsessions.class.php
@@ -6,34 +6,33 @@ class GetRoomSessions {
 
         $db = Db :: singleton();
 
-        $sql = "SELECT * FROM room INNER JOIN roomsession ON (room.Id=roomsession.RoomId) WHERE room.Id=$id";
+        $sql = "SELECT * FROM room INNER JOIN roomsession ON (room.Id=roomsession.RoomId) WHERE room.Id=$id ORDER BY roomsession.EndDate DESC";
       
+        global $sessions;
+
         $sessions= $db -> select_sql($sql);
-
-
-        
+             
         $sql2 = "SELECT * FROM room WHERE Id=$id";
         
         $room= $db -> select_sql($sql2);
         
         $RoomSessions['Name']=$room[0]['Name'];
         $RoomSessions['SiteId']=$room[0]['Id'];
-        
+
+        for ($i=0;$i<count($sessions);$i++){
+
+            $RoomSessionId = $sessions[$i]['Id'];
+
+            $sql3 = "SELECT * FROM gamesession WHERE RoomSessionId=$RoomSessionId ORDER BY EndDate DESC";
+
+            $GameSessions= $db -> select_sql($sql3);
+
+            $sessions[$i]['GameSession']=$GameSessions;
+            
+        }
+
         $RoomSessions['RoomSessions']=$sessions;
         
-        
-        foreach ($RoomSessions['RoomSessions'] as $Session){
-            
-            $SessionId=$Session['Id'];
-            
-            
-            $sql3 ="SELECT * FRom gamesession Where RoomSessionId=$SessionId";
-            $GameSessions = $db -> select_sql($sql3);
-           
-            array_push( $Session,[1,2,3]);
-           
-
-        }
         echo json_encode($RoomSessions);
         
     }
--- room.js
+++ room.js
@@ -2,19 +2,33 @@ var tableau_Missions=[];
 
 var tableau_RoomSessions=[];
 
+var tableau_GameSessions=[];
+
 
 var template_Missions=`                                         
                       <div class="text-center col-3">%MissionName%</div>
                     `;
 
-var template_Sessions=`
-                        <div class="d-flex justify-content-between my-2 GameSession" onclick="ToGame(%SessionId%)">                                    
-                            <div class="text-center col-3">%SessionId%</div>
-                            <div class="text-center col-3">%StartDate%</div>
-                            <div class="text-center col-3">%EndDate%</div>
-                            <div class="text-center col-3">%IsActive%</div>
+var template_RoomSessions=`
+                        <div class="d-flex justify-content-between my-2 GameSession">                                    
+                            <div class="text-center col-2">%SessionId%</div>
+                            <div class="text-center col-2">%StartDate%</div>
+                            <div class="text-center col-2">%EndDate%</div>
+                            <div class="text-center col-2">%IsActive%</div>
+                            <button class="text-center col-2" onclick="ShowGames(%SessionId%)">Voir les parties</button>
                         </div>
+                            <div class="d-none" id="%SessionId%">
                   `;
+
+var template_GameSessions=`
+                  <div class="d-flex justify-content-between my-2 GameSession">                                    
+                      <div class="text-center col-2">%SessionId%</div>
+                      <div class="text-center col-2">%StartDate%</div>
+                      <div class="text-center col-2">%EndDate%</div>
+                      <div class="text-center col-2">%IsActive%</div>
+                      <div class="text-center col-2">%MissionId%</div>
+                  </div>
+            `;
 
 var RoomId=location.hash.substring(1,10);
 
@@ -39,7 +53,8 @@ function getRoomSession(RoomId){
     httpRequest.onload = () => {
         tableau_RoomSessions = JSON.parse(httpRequest.responseText);
         console.log(tableau_RoomSessions);
-        Showtableau_RoomSessions();   
+        Showtableau_RoomSessions();
+        Showtableau_GameSessions();   
     };
     httpRequest.send();
 }
@@ -50,7 +65,7 @@ function Showtableau_RoomSessions(){
 
     document.getElementById("RoomName").innerHTML=tableau_RoomSessions['Name'];
 
-    document.getElementById("Sessions").innerHTML="";
+    document.getElementById("RoomSessions").innerHTML="";
 
     ShowNbreGameSessions();
 
@@ -60,16 +75,57 @@ function Showtableau_RoomSessions(){
     
     for (let i = 0; i < tableau_RoomSessions['RoomSessions'].length; i++) {
 
-        var html = template_Sessions
+        var html = template_RoomSessions
             .replaceAll("%SessionId%", tableau_RoomSessions['RoomSessions'][i].Id)
             .replaceAll("%StartDate%", tableau_RoomSessions['RoomSessions'][i].StartDate)
             .replaceAll("%EndDate%", tableau_RoomSessions['RoomSessions'][i].EndDate)
             .replaceAll("%IsActive%", tableau_RoomSessions['RoomSessions'][i].IsActive)
         
             var elt = document.createElement("div");
-            document.getElementById("Sessions").appendChild(elt);
+            document.getElementById("RoomSessions").appendChild(elt);
             elt.outerHTML = html;  
     
+    } 
+}
+
+function FillTableauGameSessions(){
+    for (var i=0;i<tableau_RoomSessions['RoomSessions'].length;i++){
+        
+        for (var j=0;j<tableau_RoomSessions['RoomSessions'][i]['GameSession'].length;j++){
+            tableau_GameSessions.push(tableau_RoomSessions['RoomSessions'][i]['GameSession'][j]);
+        }
+    }
+    console.log(tableau_GameSessions);
+}
+
+function Showtableau_GameSessions(){
+
+    document.getElementById("RoomId").innerHTML=tableau_RoomSessions['SiteId'];
+
+    document.getElementById("RoomName").innerHTML=tableau_RoomSessions['Name'];
+
+    document.getElementById("GameSessions").innerHTML="";
+
+    document.getElementById("NbreGameSessionsMonth").innerHTML="";
+    
+    document.getElementById("NbreGameSessionsDay").innerHTML="";
+    
+    FillTableauGameSessions();
+    
+    ShowNbreGameSessions();
+
+    for (let i = 0; i < tableau_GameSessions.length; i++) {
+
+        var html = template_GameSessions
+            .replaceAll("%SessionId%", tableau_GameSessions[i].Id)
+            .replaceAll("%StartDate%", tableau_GameSessions[i].StartDate)
+            .replaceAll("%EndDate%", tableau_GameSessions[i].EndDate)
+            .replaceAll("%IsActive%", tableau_GameSessions[i].IsActive)
+            .replaceAll("%MissionId%", tableau_GameSessions[i].MissionId)
+        
+            var elt = document.createElement("div");
+            document.getElementById("GameSessions").appendChild(elt);
+            elt.outerHTML = html;  
     } 
 }  
 
@@ -91,6 +147,21 @@ function ToGame(id){
 }
 
 function ShowNbreGameSessions(){
-    document.getElementById("NbreGameSessions").innerHTML="";
+    document.getElementById("NbreGameSessions").innerHTML=tableau_GameSessions.length;
+
 }
 
+function ShowGames(id){
+
+    document.getElementById(id).classList.replace("d-none","d-flex");
+    document.getElementById(id).innerHTML=id;
+    console.log(id);
+}
+
+function ShowGameSessions(){
+    document.getElementById("GameSessionsButton").classList.add("border");
+}
+
+function ShowRoomSessions(){
+    document.getElementById("RoomSessionsButton").classList.add("border");
+}
